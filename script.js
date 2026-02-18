778b011eac8f5c2effb768a2788dce25a2b32c4b
Clement Zhou <clmementzhou2@gmail.com>
2026-02-18 14:41:51 -0500

    add functions for buttons

---
+++ script.js
@@ -0,0 +1,34 @@
+let num1 = ``;
+let num2 = ``;
+let operator = ``;
+
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a ,b) {
+    return a - b;
+}
+
+const multipy = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    if (b == 0) {
+        return Error;
+    }
+    return a / b;
+}
+
+const operate = function(a, b, method) {
+    if (method == "+") {
+        add(a, b);
+    } else if (method == "-") {
+        subtract(a, b);
+    } else if (method =="x") {
+        multipy(a, b);
+    } else if(method == "/") {
+        divide(a, b);
+    }
+}
