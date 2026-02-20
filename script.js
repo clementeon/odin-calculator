eb25357026c8505ee7522f9e93d48206bf8f08cf
Clement Zhou <clmementzhou2@gmail.com>
2026-02-19 22:17:51 -0500

    add key value pairs for buttons

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-const values = {
-    num1,
-    num2,
-    operator,
+const display = {
+    num1: 0,
+    num2: 1,
+    operator: 2 ,
+    decimalAvailable: true,
 }
+
+const values = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    zero: 0,
+    decimal: ".",
+    equals: "=",
+    clear : "clear",
+    clearAll: "clearAll",
+    backspace: "backspace",
+    plus: "+",
+    minus: "-",
+    times: "x",
+    divide: "/",   
+}
+
+const buttons = document.querySelectorAll('button');
+const numLog = document.querySelector('numLog');
+const answer = document.querySelector('answer');
+
 const add = function(a, b) {
     return a + b;
 }
@@ -33,5 +61,13 @@ const operate = function(a, b, method) {
         divide(a, b);
     }
 }
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        temp = values[button.id]
+        console.log(button.id);
+        console.log(typeof(temp));
+    })
+})
 
 
